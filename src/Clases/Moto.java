40e3d569952d2a4e4048841504470e63db94086d
Jesús De La Hoz <jesusdelahoz02@gmail.com>
2026-09-17 20:15:17 -0500

    Correciones

--- src/Clases/Moto.java
+++ src/Clases/Moto.java
@@ -1,5 +1,4 @@
 package Clases;
-//arnold
 public class Moto extends Vehiculo{
     public Moto(String marca, String modelo, Double tarifaBase){
     
@@ -8,8 +7,9 @@ public class Moto extends Vehiculo{
     }
 
     @Override
-    public void calcularCostoAlquiler(int dias) {
-        
+    public double calcularCostoAlquiler(int dias) {
+        double costo = (tarifaBase * dias) + 5;
+        return costo;
     }
     
 }
