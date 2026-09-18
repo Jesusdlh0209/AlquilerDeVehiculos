c6610fdd39821c33c134c9e43b22217d8d352270
Jesús De La Hoz <jesusdelahoz02@gmail.com>
2026-09-17 19:10:15 -0500

    Clase padre lista

--- src/Clases/Vehiculo.java
+++ src/Clases/Vehiculo.java
@@ -1,6 +1,36 @@
 
 package Clases;
 
-public class Vehiculo {
+public abstract class Vehiculo {
+    protected String marca;
+    protected String modelo;
+    protected Double tarifaBase;
+        public Vehiculo() {
+    }
+
+    public Vehiculo(String marca, String modelo, Double tarifaBase) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.tarifaBase = tarifaBase;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public Double getTarifaBase() {
+        return tarifaBase;
+    }
+    public abstract void calcularCostoAlquiler(int dias);
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" + "marca=" + marca + ", modelo=" + modelo + ", tarifaBase=" + tarifaBase + '}';
+    }
     
 }
+
